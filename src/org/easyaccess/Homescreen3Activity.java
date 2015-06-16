85e00a58f450f6ca67f4383cd80ac22213f0af1d
Vishal Kharat <Vishal.Kharat@extentia.com>
2015-06-16 16:13:17 +0530

    - Solved bugs from buglist.

--- src/org/easyaccess/Homescreen3Activity.java
+++ src/org/easyaccess/Homescreen3Activity.java
@@ -16,25 +16,30 @@
  */
 package org.easyaccess;
 
+import java.util.List;
+
 import org.easyaccess.settings.SettingsMenu;
 import org.easyaccess.simplemenus.CameraAppsMenu;
 import org.easyaccess.simplemenus.SocialNetworksMenu;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.KeyListener;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-
-public class Homescreen3Activity extends AbstractHomescreenActivity implements KeyListener {
+public class Homescreen3Activity extends AbstractHomescreenActivity implements
+		KeyListener {
 	/**
 	 * The HomeScreenActivity displays the options available in the app.
 	 */
-
 
 	/** Create the Main activity showing home screen #3 **/
 	@Override
@@ -45,54 +50,97 @@ public class Homescreen3Activity extends AbstractHomescreenActivity implements K
 
 		// Launch respective easyaccess app, depending on which button is
 		// pressed
-		attachListenerToOpenExternalApp((Button) v.findViewById(R.id.btnFileBrowser), "com.rhmsoft.fm");
-		attachListenerToOpenActivity((Button) v.findViewById(R.id.btnCameraApps), CameraAppsMenu.class);
-		attachListenerToOpenActivity((Button) v.findViewById(R.id.btnSocialNetworks), SocialNetworksMenu.class);
-		attachListenerToOpenActivity((Button) v.findViewById(R.id.btnSettings), SettingsMenu.class);
+		/*
+		 * attachListenerToOpenExternalApp( (Button)
+		 * v.findViewById(R.id.btnFileBrowser), "com.rhmsoft.fm");
+		 */
+		Button btnFileBrowser = (Button) v.findViewById(R.id.btnFileBrowser);
+		btnFileBrowser.setOnClickListener(new OnClickListener() {
 
-        /** Find UI elements **/
-        Button btnMoreAccessibleApps = (Button) v.findViewById(R.id.btnMoreAccessibleApps);
+			@Override
+			public void onClick(View v) {
 
-        /** If More Accessible Apps button is pressed, open browser with accessible apps page of Eyes-Free project **/
-        btnMoreAccessibleApps.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Currently uses default browser; in future, given that Chrome will be best browser to ensure
-                // accessibility features are built in, should open the page specifically in Chrome
-                showMoreAccessibleApps();
-            }
-        });
+				final PackageManager packageManager = getActivity()
+						.getPackageManager();
+				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+				intent.setType("file/*");
+				List<ResolveInfo> list = packageManager.queryIntentActivities(
+						intent, PackageManager.GET_ACTIVITIES);
 
-        attachListenerToOpenActivity((Button) v.findViewById(R.id.btnAllApps), AllAppsActivity.class);
-        
+				if (list.size() > 0) {
+					/*Toast.makeText(getActivity(), "Apps installed",
+							Toast.LENGTH_LONG).show();*/
+					startActivity(intent);
+				} else {
+					/*
+					 * Toast.makeText(getActivity(), "No Apps installed",
+					 * Toast.LENGTH_LONG).show();
+					 */
+					launchOrDownloadFromFragment("com.rhmsoft.fm");
+				}
+			}
+
+		});
+		attachListenerToOpenActivity(
+				(Button) v.findViewById(R.id.btnCameraApps),
+				CameraAppsMenu.class);
+		attachListenerToOpenActivity(
+				(Button) v.findViewById(R.id.btnSocialNetworks),
+				SocialNetworksMenu.class);
+		attachListenerToOpenActivity((Button) v.findViewById(R.id.btnSettings),
+				SettingsMenu.class);
+
+		/** Find UI elements **/
+		Button btnMoreAccessibleApps = (Button) v
+				.findViewById(R.id.btnMoreAccessibleApps);
+
+		/**
+		 * If More Accessible Apps button is pressed, open browser with
+		 * accessible apps page of Eyes-Free project
+		 **/
+		btnMoreAccessibleApps.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Currently uses default browser; in future, given that Chrome
+				// will be best browser to ensure
+				// accessibility features are built in, should open the page
+				// specifically in Chrome
+				showMoreAccessibleApps();
+			}
+		});
+
+		attachListenerToOpenActivity((Button) v.findViewById(R.id.btnAllApps),
+				AllAppsActivity.class);
+
 		/** Put most everything before here **/
 		return v;
 	}
 
-    void showMoreAccessibleApps() {
-        Uri websiteUri = Uri.parse("http://eyes-free.googlecode.com/svn/trunk/documentation/android_access/apps.html");
-        Intent intent = new Intent(Intent.ACTION_VIEW, websiteUri);
-        startActivity(intent);
-    }
+	void showMoreAccessibleApps() {
+		Uri websiteUri = Uri
+				.parse("http://eyes-free.googlecode.com/svn/trunk/documentation/android_access/apps.html");
+		Intent intent = new Intent(Intent.ACTION_VIEW, websiteUri);
+		startActivity(intent);
+	}
 
-    @Override
-    void startSelectedActivity(View view) {
-        switch (view.getId()) {
-            case R.id.btnFileBrowser:
-                launchOrDownloadFromFragment("com.rhmsoft.fm");
-                break;
-            case R.id.btnCameraApps:
-                startNewActivity(CameraAppsMenu.class);
-                break;
-            case R.id.btnSocialNetworks:
-                startNewActivity(SocialNetworksMenu.class);
-                break;
-            case R.id.btnSettings:
-                startNewActivity(SettingsMenu.class);
-                break;
-            case R.id.btnMoreAccessibleApps:
-                showMoreAccessibleApps();
-                break;
-        }
-    }
+	@Override
+	void startSelectedActivity(View view) {
+		switch (view.getId()) {
+		case R.id.btnFileBrowser:
+			launchOrDownloadFromFragment("com.rhmsoft.fm");
+			break;
+		case R.id.btnCameraApps:
+			startNewActivity(CameraAppsMenu.class);
+			break;
+		case R.id.btnSocialNetworks:
+			startNewActivity(SocialNetworksMenu.class);
+			break;
+		case R.id.btnSettings:
+			startNewActivity(SettingsMenu.class);
+			break;
+		case R.id.btnMoreAccessibleApps:
+			showMoreAccessibleApps();
+			break;
+		}
+	}
 
 }
